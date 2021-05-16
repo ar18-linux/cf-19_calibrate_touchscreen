b17b745daef3a2606b229f1d0beafd295055c51b
nulysses <adr.zivil@gmail.com>
2021-05-16 13:41:07 +0200

    1

--- install.sh
+++ install.sh
@@ -18,3 +18,10 @@ fi
 rm -rf "${install_dir}/${module_name}"
 cp -rf "${script_dir}/${module_name}" "${install_dir}/${module_name}"
 chmod +x "${install_dir}/${module_name}/"* -R
+
+if [ -d "/home/${user_name}/.config/ar18/autostarts" ]; then
+  auto_start="/home/${user_name}/.config/ar18/autostarts/${module_name}.sh"
+  cp "${script_dir}/${module_name}/startup.sh" "${auto_start}"
+  echo "LD_PRELOAD='' ${install_dir}/${module_name}/${module_name}.sh" >> "${auto_start}"
+  chmod +x "${auto_start}"
+fi
